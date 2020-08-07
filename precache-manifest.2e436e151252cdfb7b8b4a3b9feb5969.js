c5b71844290434ae35a7bc46951ff11b1f9a5af2
James Crews <jcrews206@gmail.com>
2020-08-07 14:16:11 -0700

    Updates

---
+++ precache-manifest.2e436e151252cdfb7b8b4a3b9feb5969.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba71514cafff10871224ebd210aaf0ce",
+    "url": "/chess/index.html"
+  },
+  {
+    "revision": "5c7343d7e57fe0503006",
+    "url": "/chess/static/css/main.f4192bf8.chunk.css"
+  },
+  {
+    "revision": "653d89ebbccc6fa29345",
+    "url": "/chess/static/js/2.dbc64c88.chunk.js"
+  },
+  {
+    "revision": "99c1bf48081ed736382f4cc79d8323d8",
+    "url": "/chess/static/js/2.dbc64c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c7343d7e57fe0503006",
+    "url": "/chess/static/js/main.ec7a5d07.chunk.js"
+  },
+  {
+    "revision": "fe54e1bce9cc9d1a3729",
+    "url": "/chess/static/js/runtime-main.bff3f106.js"
+  },
+  {
+    "revision": "8b2b12c2d1234fa15dd1b1594c8999c8",
+    "url": "/chess/static/media/b_bishop.8b2b12c2.png"
+  },
+  {
+    "revision": "fa711b01377bda96600b321922acfe59",
+    "url": "/chess/static/media/b_king.fa711b01.png"
+  },
+  {
+    "revision": "392a8a975c69865112514dea0818e626",
+    "url": "/chess/static/media/b_knight.392a8a97.png"
+  },
+  {
+    "revision": "33c6643b3cd0915bcf3a806195b79b71",
+    "url": "/chess/static/media/b_pawn.33c6643b.png"
+  },
+  {
+    "revision": "46665aba4d919e2030c8afa6108472d4",
+    "url": "/chess/static/media/b_queen.46665aba.png"
+  },
+  {
+    "revision": "9d137f9062f676259dffc68062272619",
+    "url": "/chess/static/media/w_bishop.9d137f90.png"
+  },
+  {
+    "revision": "820630db3e77984665c3a809d7842910",
+    "url": "/chess/static/media/w_king.820630db.png"
+  },
+  {
+    "revision": "2415f9b8f69f24f517fb330656615905",
+    "url": "/chess/static/media/w_knight.2415f9b8.png"
+  },
+  {
+    "revision": "5c54df9e901767511e1ad52254583213",
+    "url": "/chess/static/media/w_queen.5c54df9e.png"
+  }
+]);
