2c297caeea0f7dcbbce0e9d0b55df1033f5c8454
JCrews253 <jcrews206@gmail.com>
2020-08-10 14:33:21 -0700

    added legend

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,31 @@ const Header = () => {
 
     return(
         <div className='Header__container'>
-            <h1>Chess AI with Min Max Algorithm</h1>
+            <div className='Header__title-container'>
+                <h1>Chess AI with Min Max Algorithm</h1>
+            </div>
+            <div className='Header__legend-container'>
+                <div className='Header__legend-item'>
+                    <div className='Header__selected-index'/>
+                    <p>Selected Index</p>
+                </div>
+                <div className='Header__legend-item'>
+                    <div className='Header__available-move'/>
+                    <p>Available Move</p>
+                </div>
+                <div className='Header__legend-item'>
+                    <div className='Header__killing-move'/>
+                    <p>Taking Move</p>
+                </div>
+                <div className='Header__legend-item'>
+                    <div className='Header__ai-start-pos'/>
+                    <p>AI Turn Start Position</p>
+                </div>
+                <div className='Header__legend-item'>
+                    <div className='Header__ai-finish-pos'/>
+                    <p>AI Turn End Position</p>
+                </div>
+            </div>
         </div>
     )
 }
